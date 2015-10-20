6985344b143bc07ade608d8bed93580de2d3ecd4
Valball <val_jub@live.fr>
2015-10-20 15:26:27 +0200

    Some try to config IDE
    Some try to config IDE

--- Remotecontrol_android/app/src/main/java/src/remotecontrol_android/model/message/Action.java
+++
@@ -1,7 +0,0 @@
-package src.remotecontrol_android.model.message;
-
-/**
- * Created by cyprien on 08/10/15.
- */
-public enum Action { NONE
-}
--- Remotecontrol_pc/src/model/message/Action.java
+++
@@ -1,7 +0,0 @@
-package model.message;
-
-/**
- * Created by cyprien on 08/10/15.
- */
-public enum Action { NONE
-}
