28882a41328a2ef2f8735fb1f2a37c287aee7cc2
Cyprien RUFFINO <ruffino.cyprien@gmail.com>
2016-01-29 13:27:21 +0100

    Ajout du swipe sur les diapos

--- android/app/src/main/java/view/DiapoActivity.java
+++ android/app/src/main/java/view/DiapoActivity.java
@@ -1,10 +1,16 @@
 package view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.EditText;
+
 import controller.Controller;
 import controller.communication.callbackInterface.ErrorInterface;
 import controller.communication.events.DiapoEvent;
@@ -23,6 +29,74 @@ public class DiapoActivity extends Activity implements ErrorInterface {
     private TCPService tcpService;
     private SoftwareActivity.Diapo soft;
 
+    private class OnSwipeTouchListener implements OnTouchListener {
+
+        private final GestureDetector gestureDetector;
+
+        public OnSwipeTouchListener (Context ctx){
+            gestureDetector = new GestureDetector(ctx, new GestureListener());
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return gestureDetector.onTouchEvent(event);
+        }
+
+        private final class GestureListener extends SimpleOnGestureListener {
+
+            private static final int SWIPE_THRESHOLD = 100;
+            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                        }
+                        result = true;
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom();
+                        } else {
+                            onSwipeTop();
+                        }
+                    }
+                    result = true;
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        }
+
+        public void onSwipeRight() {
+        }
+
+        public void onSwipeLeft() {
+        }
+
+        public void onSwipeTop() {
+        }
+
+        public void onSwipeBottom() {
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +107,24 @@ public class DiapoActivity extends Activity implements ErrorInterface {
         if (tcpService == null) {
             runOnUiThread(new ToastRunnable(getApplicationContext(), getString(R.string.no_tcp_service)));
         }
+        findViewById(R.id.diapolayout).setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
+             @Override
+             public void onSwipeLeft() {
+                 send(new DiapoEvent(DiapoEvent.DiapoEventType.Next, soft.getId()));
+             }
+              @Override
+             public void onSwipeTop() {
+                 send(new DiapoEvent(DiapoEvent.DiapoEventType.Next, soft.getId()));
+             }
+              @Override
+             public void onSwipeBottom() {
+                 send(new DiapoEvent(DiapoEvent.DiapoEventType.Prec, soft.getId()));
+             }
+              @Override
+             public void onSwipeRight() {
+                 send(new DiapoEvent(DiapoEvent.DiapoEventType.Prec, soft.getId()));
+             }
+         });
     }
 
     public void go_to(View view) {
